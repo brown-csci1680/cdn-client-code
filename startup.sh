a37e24e10c75fa1f854641aff45c738d658f355e
KevinDackow <Kevin Dackow>
2019-12-01 17:19:24 -0500

    Add dep installation:

--- startup.sh
+++ startup.sh
@@ -3,4 +3,7 @@ rm client driver_to_client_pb2.py
 wget https://raw.githubusercontent.com/brown-csci1680/cdn-client-code/master/client
 wget https://raw.githubusercontent.com/brown-csci1680/cdn-client-code/master/driver_to_client_pb2.py
 chmod +rwx client driver_to_client_pb2.py
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+pip3 install requests
+pip3 install protobuf
 ./client
