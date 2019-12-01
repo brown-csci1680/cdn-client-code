2a24e7835ef7a897a8cbb6e6fbd14c7636af6a1a
KevinDackow <Kevin Dackow>
2019-12-01 15:58:14 -0500

    remove /usr

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#! /bin/bash
 wget https://raw.githubusercontent.com/brown-csci1680/cdn-client-code/master/client
 wget https://raw.githubusercontent.com/brown-csci1680/cdn-client-code/master/driver_to_client_pb2.py
 ./client
