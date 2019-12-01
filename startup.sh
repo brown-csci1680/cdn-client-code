b4e899900983fedfdeef50d1adee63457264ea9f
KevinDackow <Kevin Dackow>
2019-12-01 17:06:18 -0500

    Update startup script

--- startup.sh
+++ startup.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+rm client driver_to_client_pb2.py
 wget https://raw.githubusercontent.com/brown-csci1680/cdn-client-code/master/client
 wget https://raw.githubusercontent.com/brown-csci1680/cdn-client-code/master/driver_to_client_pb2.py
 chmod +rwx client driver_to_client_pb2.py
