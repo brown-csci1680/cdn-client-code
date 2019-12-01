8a7b34e0a906dffa1789c47542b0986d2636bb72
KevinDackow <Kevin Dackow>
2019-12-01 15:55:17 -0500

    update startup

--- startup.sh
+++ startup.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/bash
-wget
+wget https://raw.githubusercontent.com/brown-csci1680/cdn-client-code/master/client
+wget https://raw.githubusercontent.com/brown-csci1680/cdn-client-code/master/driver_to_client_pb2.py
+./client
