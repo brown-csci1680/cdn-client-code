581ee3142a2df00387179f939c74e457f7b596e9
KevinDackow <Kevin Dackow>
2019-12-01 19:19:26 -0500

    updated scripts

--- startup.sh
+++ startup.sh
@@ -4,6 +4,7 @@ wget https://raw.githubusercontent.com/brown-csci1680/cdn-client-code/master/cli
 wget https://raw.githubusercontent.com/brown-csci1680/cdn-client-code/master/driver_to_client_pb2.py
 chmod +rwx client driver_to_client_pb2.py
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3 get-pip.py
 pip3 install requests
 pip3 install protobuf
 ./client
